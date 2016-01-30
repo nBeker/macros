49ea1fb18b66756239c8d5ed8cc0d2d344eb8468
Nimrod Beker <nimrod.beker@gmail.com>
2016-01-30 14:39:11 +0200

    added clipboard simplifier

---
+++ simple_clipboard.py
@@ -0,0 +1,34 @@
+import win32clipboard
+
+class simple_clipboard(object):
+    def __init__(self):
+        self._is_open = False
+    
+    def set(self, data):
+        if self._is_open:
+            win32clipboard.SetClipboardText(data, win32clipboard.CF_UNICODETEXT)
+        else:
+            raise RuntimeError("Clipboard not opened")
+
+    def get(self):
+        if self._is_open:
+            return win32clipboard.GetClipboardData()
+        else:
+            raise RuntimeError("Clipboard not opened")
+
+    def empty(self):
+        if self._is_open:
+            return win32clipboard.EmptyClipboard()
+        else:
+            raise RuntimeError("Clipboard not opened")
+
+    def __enter__(self):
+        if not self._is_open:
+            win32clipboard.OpenClipboard()
+            self._is_open = True
+            return self
+
+    def __exit__(self, type, value, traceback):
+        if self._is_open:
+            win32clipboard.CloseClipboard()
+            self._is_open = False
--- text_manipulate.py
+++ text_manipulate.py
@@ -1,9 +1,14 @@
-import win32clipboard as clipboard
+from simple_clipboard import simple_clipboard
 
 import virtual_keyboard
 
+send_alt_tab = lambda: virtual_keyboard.press_combo("A\t")
+send_mark_all = lambda: virtual_keyboard.press_combo("^a")
+send_paste = lambda: virtual_keyboard.press_combo("^v")
+send_copy = lambda: virtual_keyboard.press_combo("^c")
+send_cut = lambda: virtual_keyboard.press_combo("^x")
 
-def manipulator(function, replace_source=True, empty_clipboard=False):
+def manipulator(function, replace_source=True, empty_clipboard=False, mark_all=False):
     """
     The manipulator function cuts all text from the previously focused window(last foreground window before script call)
     and then performs a manipulation on the text with a given function. after the manipulation the text will be returned
@@ -13,30 +18,31 @@ def manipulator(function, replace_source=True, empty_clipboard=False):
     :param empty_clipboard: empty clipboard in-order to clean it
     """
     try:
+        # import ipdb; ipdb/set_trace()
         # Alt + Tab to return to the previous context
-        virtual_keyboard.press_combo("A\t")
+        send_alt_tab()
+        if mark_all:
+            send_mark_all()
+
         if replace_source:
-            # Ctrl+A+X to Cut all text
-            virtual_keyboard.press_combo("^ax")
+            # Cut text
+            send_cut()
         else:
-            # Ctrl+A+C to Cut all text
-            virtual_keyboard.press_combo("^ac")
+            # Copy text
+            send_copy()
 
-        clipboard.OpenClipboard()
-        data = clipboard.GetClipboardData()
-
-        if empty_clipboard:
-            clipboard.EmptyClipboard()
-
-        clipboard.SetClipboardText(function(data), clipboard.CF_UNICODETEXT)
+        with simple_clipboard() as clipboard:
+            data = clipboard.get()
+            if empty_clipboard:
+                clipboard.empty()
+            clipboard.set(function(data))
 
     except Exception as e:
         # Alt + Tab to return to the python context
-        virtual_keyboard.press_combo("A\t")
+        send_alt_tab()
         print(e.args)
         input()
     finally:
-        clipboard.CloseClipboard()
         if replace_source:
             # Ctrl+V to Paste the new text
-            virtual_keyboard.press_combo("^v")
+            send_paste()
