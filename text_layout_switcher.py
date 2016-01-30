bb57f4ca7a3084abaf107f328e9889692aef2001
Nimrod Beker <nimrod.beker@gmail.com>
2016-01-30 21:39:34 +0200

    added clipboard only manipulation and exported the keyboard actions to a context managed class

--- text_layout_switcher.py
+++ text_layout_switcher.py
@@ -1,16 +1,25 @@
 import text_manipulate
 import layout_switcher
 
+from sys import argv as arguments
 
-def main():
+
+
+def main(flip_clipboard=False):
     try:
-        text_manipulate.manipulator(layout_switcher.switch_layout)
+        if flip_clipboard:
+            text_manipulate.manipulator(function=layout_switcher.switch_layout)
+        else:
+            text_manipulate.manipulator_with_keyboard(function=layout_switcher.switch_layout, replace_source=True)
     except Exception as e:
-        import ipdb;
-
-        ipdb.set_trace()
+        import ipdb; ipdb.set_trace()
         input()
 
 
 if __name__ == "__main__":
-    main()
+    flip_clipboard = False
+    if len(arguments) > 1:
+        if arguments[1] in "clip":
+            flip_clipboard = True
+
+    main(flip_clipboard=flip_clipboard)
--- text_manipulate.py
+++ text_manipulate.py
@@ -2,13 +2,38 @@ from simple_clipboard import simple_clipboard
 
 import virtual_keyboard
 
-send_alt_tab = lambda: virtual_keyboard.press_combo("A\t")
-send_mark_all = lambda: virtual_keyboard.press_combo("^a")
-send_paste = lambda: virtual_keyboard.press_combo("^v")
-send_copy = lambda: virtual_keyboard.press_combo("^c")
-send_cut = lambda: virtual_keyboard.press_combo("^x")
 
-def manipulator(function, replace_source=True, empty_clipboard=False, mark_all=False):
+
+class UseKeyboard(object):
+    send_alt_tab = lambda x: virtual_keyboard.press_combo("A\t")
+    send_mark_all = lambda x: virtual_keyboard.press_combo("^a")
+    send_paste = lambda x: virtual_keyboard.press_combo("^v")
+    send_copy = lambda x: virtual_keyboard.press_combo("^c")
+    send_cut = lambda x: virtual_keyboard.press_combo("^x")
+
+    def __init__(self, replace_source=True, mark_all=False):
+        self._replace_source = replace_source
+        self._mark_all = mark_all
+
+    def __enter__(self):
+        self.send_alt_tab()
+        if self._mark_all:
+            self.send_mark_all()
+
+        if self._replace_source:
+            # Cut text
+            self.send_cut()
+        else:
+            # Copy text
+            self.send_copy()
+        return self
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self._replace_source:
+            self.send_paste()
+
+
+def manipulator(function, empty_clipboard=False):
     """
     The manipulator function cuts all text from the previously focused window(last foreground window before script call)
     and then performs a manipulation on the text with a given function. after the manipulation the text will be returned
@@ -18,31 +43,26 @@ def manipulator(function, replace_source=True, empty_clipboard=False, mark_all=F
     :param empty_clipboard: empty clipboard in-order to clean it
     """
     try:
-        # import ipdb; ipdb/set_trace()
-        # Alt + Tab to return to the previous context
-        send_alt_tab()
-        if mark_all:
-            send_mark_all()
-
-        if replace_source:
-            # Cut text
-            send_cut()
-        else:
-            # Copy text
-            send_copy()
-
         with simple_clipboard() as clipboard:
             data = clipboard.get()
             if empty_clipboard:
                 clipboard.empty()
+
             clipboard.set(function(data))
 
     except Exception as e:
-        # Alt + Tab to return to the python context
-        send_alt_tab()
         print(e.args)
         input()
-    finally:
-        if replace_source:
-            # Ctrl+V to Paste the new text
-            send_paste()
+
+
+def manipulator_with_keyboard(function, empty_clipboard=False, replace_source=True, mark_all=False):
+    """
+    The manipulator function cuts all text from the previously focused window(last foreground window before script call)
+    and then performs a manipulation on the text with a given function. after the manipulation the text will be returned
+    On case of failure, the text will be returned also.
+    :param function: the manipulation function
+    :param replace_source: replace the source text if it can
+    :param empty_clipboard: empty clipboard in-order to clean it
+    """
+    with UseKeyboard(replace_source=replace_source, mark_all=mark_all):
+        manipulator(function=function, empty_clipboard=empty_clipboard)
