fbe3741cecb30adcf6e59f232762b278c3e00072
Nimrod Beker <nimrod.beker@gmail.com>
2016-02-21 01:45:17 +0200

    ugly fixed mouse

--- scripter/play.py
+++ scripter/play.py
@@ -5,38 +5,34 @@ import json
 from time import sleep
 
 from win32con import KEYEVENTF_KEYUP as RELEASE_KEY
-import win32con
-from pyHook import HookConstants as pyhook_consts
+import pyHook
 
 
 MOUSE_EVENT = 0
 KEYBOARD_EVENT = 1
 
-MOUSE_EVENTS = {
-    pyhook_consts.WM_MBUTTONDOWN: win32con.MOUSEEVENTF_MIDDLEDOWN,
-    pyhook_consts.WM_MBUTTONUP: win32con.MOUSEEVENTF_MIDDLEUP,
-    pyhook_consts.WM_RBUTTONDOWN: win32con.MOUSEEVENTF_RIGHTDOWN,
-    pyhook_consts.WM_RBUTTONUP: win32con.MOUSEEVENTF_RIGHTUP,
-    pyhook_consts.WM_LBUTTONDOWN: win32con.MOUSEEVENTF_LEFTDOWN,
-    pyhook_consts.WM_LBUTTONUP: win32con.MOUSEEVENTF_LEFTUP
-}
-# win32con.MOUSEEVENTF_ABSOLUTE   
-# win32con.MOUSEEVENTF_XDOWN
-# win32con.MOUSEEVENTF_XUP
-# win32con.MOUSEEVENTF_MOVE       
-# win32con.MOUSEEVENTF_WHEEL
 
 def MouseAction(action):
+    """
+    Performs mouse action.
+    First, set mouse position at the required coordinate, and then operates.
+    operations means either press or de-press any mouse key
+    :param action: a tuple \ list contains the operation (key-press \ key-de-press) and cursor position
+    :return:
+    """
     operation, position = action
     set_mouse_position(position)
-    print(operation, pyhook_consts.MsgToName(operation))
-    print(position)
-    mouse_event(MOUSE_EVENTS.get(operation), 0, 0, 0, 0)
+    mouse_event(operation, 0, 0, 0, 0)
 
 
 def KeyboardAction(action):
+    """
+    Performs keyboard action (either press or de-press key)
+    :param action: a tuple \ list contains the key_id and the operation (press \ de-press)
+    :return:
+    """
     key_id, operation = action
-    if operation == pyhook_consts.WM_KEYDOWN:
+    if operation == pyHook.HookConstants.WM_KEYDOWN:
         keyboard_event(key_id, 0, 0, 0)
     else:
         keyboard_event(key_id, 0, RELEASE_KEY, 0)
--- scripter/record.py
+++ scripter/record.py
@@ -1,11 +1,19 @@
+"""
+The following script records any keyboard action and mouse operation.
+The output will be a Python file which can be executed and repeat the user input.
+
+Idea Originating Credit: A.S.
+"""
 import argparse
 import ctypes
 import json
 import os.path
+from string import Template
+from win32gui import GetCursorPos as mouse_position
 
 import pythoncom
 import pyHook
-from string import Template
+import win32con
 
 
 actions_list = list()
@@ -28,9 +36,21 @@ def handle_arguments():
     return parser.parse_args()
 
 
+# MOUSE_EVENTS contains cross mapping between pyHook to WinAPI constants
+MOUSE_EVENTS = {
+    pyHook.HookConstants.WM_MBUTTONDOWN: win32con.MOUSEEVENTF_MIDDLEDOWN,
+    pyHook.HookConstants.WM_MBUTTONUP: win32con.MOUSEEVENTF_MIDDLEUP,
+    pyHook.HookConstants.WM_RBUTTONDOWN: win32con.MOUSEEVENTF_RIGHTDOWN,
+    pyHook.HookConstants.WM_RBUTTONUP: win32con.MOUSEEVENTF_RIGHTUP,
+    pyHook.HookConstants.WM_LBUTTONDOWN: win32con.MOUSEEVENTF_LEFTDOWN,
+    pyHook.HookConstants.WM_LBUTTONUP: win32con.MOUSEEVENTF_LEFTUP
+}
+
+
 def OnMouseEvent(event):
     global actions_list
-    actions_list.append((MOUSE_EVENT, (event.Message, event.Position)))
+    # TODO: fix this - event.Position
+    actions_list.append((MOUSE_EVENT, (MOUSE_EVENTS[event.Message], mouse_position())))
 
     # return True to pass the event to other handlers
     # return False to stop the event from propagating
@@ -49,18 +69,18 @@ def OnKeyboardEvent(event):
     return True
 
 
-# create the hook mananger
-hm = pyHook.HookManager()
-# register two callbacks
-hm.MouseAllButtons = OnMouseEvent
-hm.KeyAll = OnKeyboardEvent
-
-# hook into the mouse and keyboard events
-hm.HookMouse()
-hm.HookKeyboard()
-
 if __name__ == "__main__":
     args = handle_arguments()
+
+    # create the hook mananger
+    hm = pyHook.HookManager()
+    # register two callbacks
+    hm.MouseAllButtons = OnMouseEvent
+    hm.KeyAll = OnKeyboardEvent
+
+    # hook into the mouse and keyboard events
+    hm.HookMouse()
+    hm.HookKeyboard()
 
     pythoncom.PumpMessages()
 
@@ -68,7 +88,7 @@ if __name__ == "__main__":
         # Saving the information:
 
         with open("template.py") as t:
-            template =  Template(t.read())
+            template = Template(t.read())
 
         with open(args.macro_name, 'w') as f:
             f.write(template.substitute(actions_json=json.dumps(actions_list)))
