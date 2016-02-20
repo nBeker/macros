d3327ebc5dd81b6431c0bc15a0a1177a26ebe854
Nimrod Beker <nimrod.beker@gmail.com>
2016-02-20 16:51:48 +0200

    added scripter - a True-Macro Creater recrods keyboard and mouse actions and repeats them

---
+++ scripter/play.py
@@ -0,0 +1,54 @@
+from win32api import keybd_event as keyboard_event
+from win32api import mouse_event
+from win32api import SetCursorPos as set_mouse_position
+import json
+from time import sleep
+
+from win32con import KEYEVENTF_KEYUP as RELEASE_KEY
+import win32con
+from pyHook import HookConstants as pyhook_consts
+
+
+MOUSE_EVENT = 0
+KEYBOARD_EVENT = 1
+
+MOUSE_EVENTS = {
+    pyhook_consts.WM_MBUTTONDOWN: win32con.MOUSEEVENTF_MIDDLEDOWN,
+    pyhook_consts.WM_MBUTTONUP: win32con.MOUSEEVENTF_MIDDLEUP,
+    pyhook_consts.WM_RBUTTONDOWN: win32con.MOUSEEVENTF_RIGHTDOWN,
+    pyhook_consts.WM_RBUTTONUP: win32con.MOUSEEVENTF_RIGHTUP,
+    pyhook_consts.WM_LBUTTONDOWN: win32con.MOUSEEVENTF_LEFTDOWN,
+    pyhook_consts.WM_LBUTTONUP: win32con.MOUSEEVENTF_LEFTUP
+}
+# win32con.MOUSEEVENTF_ABSOLUTE   
+# win32con.MOUSEEVENTF_XDOWN
+# win32con.MOUSEEVENTF_XUP
+# win32con.MOUSEEVENTF_MOVE       
+# win32con.MOUSEEVENTF_WHEEL
+
+def MouseAction(action):
+    operation, position = action
+    set_mouse_position(position)
+    print(operation, pyhook_consts.MsgToName(operation))
+    print(position)
+    mouse_event(MOUSE_EVENTS.get(operation), 0, 0, 0, 0)
+
+
+def KeyboardAction(action):
+    key_id, operation = action
+    if operation == pyhook_consts.WM_KEYDOWN:
+        keyboard_event(key_id, 0, 0, 0)
+    else:
+        keyboard_event(key_id, 0, RELEASE_KEY, 0)
+
+
+actions = {MOUSE_EVENT: MouseAction,
+           KEYBOARD_EVENT: KeyboardAction}
+
+
+def play_macro(macro_json, action_delay=.1, animate_mouse=False):
+    # Loading the macro
+    actions_list = json.loads(macro_json)
+    for action in actions_list:
+        actions[action[0]](action[1])
+        sleep(action_delay)
---
+++ scripter/record.py
@@ -0,0 +1,71 @@
+import argparse
+import ctypes
+import json
+import os.path
+
+import pythoncom
+import pyHook
+
+
+actions_list = list()
+MOUSE_EVENT = 0
+KEYBOARD_EVENT = 1
+
+
+def handle_arguments():
+    def macros_file(path):
+        basename = os.path.basename(path)
+        if not basename.endswith(".py"):
+            basename += ".py"
+        return basename
+
+
+    parser = argparse.ArgumentParser(prog="Macros Creator",
+                                     description="Create macros for repeated use")
+    parser.add_argument("macro_name", help="The macro name", type=macros_file)
+
+    return parser.parse_args()
+
+
+def OnMouseEvent(event):
+    global actions_list
+    actions_list.append((MOUSE_EVENT, (event.Message, event.Position)))
+
+    # return True to pass the event to other handlers
+    # return False to stop the event from propagating
+    return True
+
+
+def OnKeyboardEvent(event):
+    if event.KeyID is 35:
+        ctypes.windll.user32.PostQuitMessage(0)
+        return True
+
+    global actions_list
+    actions_list.append((KEYBOARD_EVENT, (event.KeyID, event.Message)))
+    # return True to pass the event to other handlers
+    # return False to stop the event from propagating
+    return True
+
+
+# create the hook mananger
+hm = pyHook.HookManager()
+# register two callbacks
+hm.MouseAllButtons = OnMouseEvent
+hm.KeyAll = OnKeyboardEvent
+
+# hook into the mouse and keyboard events
+hm.HookMouse()
+hm.HookKeyboard()
+
+if __name__ == "__main__":
+    args = handle_arguments()
+
+    pythoncom.PumpMessages()
+
+    if actions_list:
+        # Saving the information:
+        with open(args.macro_name, 'w') as f:
+            f.write("import play\n"
+                    "play.play_macro('{actions_json}')".format(actions_json=json.dumps(actions_list)))
+
